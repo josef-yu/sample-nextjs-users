396286924f47028c7d0b53cb53136438bbb137ae
josef-yu <slay3r28@gmail.com>
2023-01-03 23:09:30 +0800

    Update img to Image with alt property

--- components/User.tsx
+++ components/User.tsx
@@ -1,10 +1,17 @@
 import type { User } from '../interfaces/user';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function UserComponent(props: User) {
 	return (
 		<div className='flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl py-4 px-1'>
-			<img src={props.avatar} className='object-cover rounded-full w-14 h-14' />
+			<Image
+				className='object-cover rounded-full w-14 h-14'
+				src={props.avatar}
+				alt={props.first_name}
+				width={128}
+				height={128}
+			/>
 			<div className='flex flex-col justify-between p-4 leading-normal w-full text-center md:text-start'>
 				<div className='font-semibold text-md'>{`${props.first_name} ${props.last_name}`}</div>
 				<Link className='text-sm underline' href={`mailto:${props.email}`}>
